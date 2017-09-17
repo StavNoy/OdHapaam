e5b1e753127f09138b4c9c5d51673d1166f6fb1d
nikita <nikita.kurtin@gmail.com>
2017-09-17 15:16:07 +0300

    add mainScreen and graphics

---
+++ app/src/main/java/project/stav/odhapaam2/myButtons/MainScreen.java
@@ -0,0 +1,40 @@
+package project.stav.odhapaam2.myButtons;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.widget.Button;
+import android.widget.GridLayout;
+
+import java.util.Random;
+
+
+public class MainScreen extends AppCompatActivity {
+    final int [] buttons= new int[]{R.layout.button1,R.layout.button2,R.layout.button3};
+    Button[] [] candies=new Button[5][5];
+    GridLayout main;
+    int i;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        main = (GridLayout)findViewById(R.id.main);
+        creatingButtons();
+    }
+    private void creatingButtons(){
+        for (int x = 0;x<candies.length;x++){
+            for (int y = 0;y<candies[x].length;y++){
+                candies[x][y]=randomize();
+                main.addView(candies[x][y],185,230);
+
+
+            }
+        }
+
+    }
+    private Button randomize(){
+        Button b =(Button) LayoutInflater.from(this).inflate(buttons[new Random().nextInt(3)],null);
+        return b;
+    }
+}
+
