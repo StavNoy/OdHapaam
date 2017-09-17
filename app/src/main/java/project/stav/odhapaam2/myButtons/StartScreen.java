f642b2dd5593351fa73e9a9aff0c6c645b993637
nikita <nikita.kurtin@gmail.com>
2017-09-17 15:36:38 +0300

    Oran is in the game bitch!

---
+++ app/src/main/java/project/stav/odhapaam2/myButtons/StartScreen.java
@@ -0,0 +1,56 @@
+package project.stav.odhapaam2.myButtons;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class StartScreen extends AppCompatActivity {
+
+    ImageView img;
+    Button button;
+    Uri imageUri;
+    private static final int PICK_IMAGE = 100;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        img = (ImageView)findViewById(R.id.image);
+        button = (Button) findViewById(R.id.btn);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openGallery();
+            }
+        });
+    }
+
+
+
+    private void openGallery(){
+        Intent gallery = new Intent(Intent.ACTION_PICK , MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery , PICK_IMAGE);
+    }
+
+
+
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == RESULT_OK && requestCode == PICK_IMAGE){
+            imageUri = data.getData();
+            img.setImageURI(imageUri);
+        }
+    }
+}
