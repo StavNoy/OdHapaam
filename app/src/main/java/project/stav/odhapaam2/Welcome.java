0ee1dce816db3c12e71eacb723a77d34ad5b7c14
nikita <nikita.kurtin@gmail.com>
2017-09-19 11:49:01 +0300

    Merged

--- app/src/main/java/project/stav/odhapaam2/Welcome.java
+++ app/src/main/java/project/stav/odhapaam2/Welcome.java
@@ -2,11 +2,14 @@ package project.stav.odhapaam2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class Welcome extends AppCompatActivity {
 
     TextView welcome;
+    EditText user;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +17,10 @@ public class Welcome extends AppCompatActivity {
         setContentView(R.layout.activity_welcome);
 
         welcome = (TextView) findViewById(R.id.welcome);
+        user = (EditText) findViewById(R.id.input1);
+
+
     }
+
+
 }
