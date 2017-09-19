335b73d5cb1848626df2d3c695fc016ed06d5314
nikita <nikita.kurtin@gmail.com>
2017-09-19 14:19:47 +0300

    Merge branch 'master' of C:\Users\hackeru\Desktop\stav\OdHapaam2 with conflicts.

--- app/src/main/java/project/stav/odhapaam2/MySHaredPreferences.java
+++ app/src/main/java/project/stav/odhapaam2/MySHaredPreferences.java
@@ -3,12 +3,16 @@ package project.stav.odhapaam2;
 import android.content.SharedPreferences;
 import android.net.Uri;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by hackeru on 19/09/2017.
  */
 
 public class MySharedPreferences {
-    SharedPreferences prefs;
+    private SharedPreferences prefs;
 
     public MySharedPreferences(SharedPreferences prefs) {
         this.prefs = prefs;
@@ -17,10 +21,23 @@ public class MySharedPreferences {
         return prefs.getInt("score",0);
     }
     public void setScore(int newScore){
-        if (newScore>=0) prefs.edit().putInt("score",newScore);
+        if (newScore>=0) prefs.edit().putInt("score",newScore).apply();
     }
     public Uri [] getImages(){
-        return prefs.getString("images","android.resource://[project.stav.odhapaam2]/[R.drawable.triangle]");
+        Uri [] images=new Uri[4];
+        Set<String> prefImgs = prefs.getStringSet("images",new HashSet<String>(4));
+        for(int i =0 ; i<images.length ; i++){
+            images[i]=Uri.parse(prefImgs.iterator().next());
+        }
+        return images;
+    }
+    public void saveImages(Uri[] uris){
+        Set<String> toSave=new HashSet<>();
+        for (Uri u : uris){
+            toSave.add(u.toString());
+        }
+        prefs.edit().putStringSet("images",toSave).apply();
+
     }
 
 }
